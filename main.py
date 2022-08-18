77c19011a888f5a88ca153be8e0db991fcc99372
José Fernando Chávez Castillo <josefernandochca@gmail.com>
2022-08-17 20:49:18 -0500

    Update

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ Fecha_actual = current_date_format(datetime.datetime.now())
 
 @bot.slash_command(guild_ids=[ServersID], name='ip-server', description="Mira la ip del servidor!")
 async def Ip_servidor(interaction: Interaction):
-    embed=nextcord.Embed(title="IP:", description="Servidor en proceso..", color=0x1FD3F3)
+    embed=nextcord.Embed(title="IP:", description="Servidor en proceso...", color=0x1FD3F3)
     await interaction.response.send_message(embed=embed)
 
 @bot.slash_command(guild_ids=[ServersID], name='profile-user', description="Mira el perfil de un usuario!")
