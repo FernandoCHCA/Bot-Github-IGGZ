6c776c540366146b90a35722e2af0368883f786f
José Fernando Chávez Castillo <josefernandochca@gmail.com>
2022-08-18 23:29:36 -0500

    Update

--- cogs/server-info.py
+++ cogs/server-info.py
@@ -12,11 +12,13 @@ class ServerInfo(commands.Cog):
         role_count = len(interaction.guild.roles)
         list_of_bots = [bot.mention for bot in interaction.guild.members if bot.bot]
         embed = nextcord.Embed(color=0x1FD3F3)
+        Contador_humanos = [interaction.guild.humans.count]
+        Contador_bots = [interaction.guild.bots.count]
         embed.set_author(name=interaction.guild)
         embed.set_thumbnail(interaction.guild.icon)
         embed.add_field(name='Owner', value=interaction.guild.owner, inline=False)
         embed.add_field(name='Creado', value=interaction.guild.created_at.__format__('%d/%m/%Y, %H:%M:%S PM'), inline=False)
-        embed.add_field(name='Contador de Miembros', value='{} humanos | {} bots | {} total'.format(interaction.guild.humans.count, interaction.guild.bots.count, interaction.guild.member_count), inline=False)
+        embed.add_field(name='Contador de Miembros', value='{} humanos | {} bots | {} total'.format(Contador_humanos, Contador_bots, interaction.guild.member_count), inline=False)
         embed.add_field(name='Top rol', value=interaction.guild.roles[-31])
         embed.add_field(name='Bots', value=list_of_bots, inline=False)
         embed.set_footer(text=f'Requested by: {interaction.user.name}#{interaction.user.discriminator}\n{Fecha_actual}', icon_url='https://i.ibb.co/nCCJ2Wb/378-3782140-discord-server-icon-cute-imagenes-para-perfil-de.png')
