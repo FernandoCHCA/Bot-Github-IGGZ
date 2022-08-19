09181bd271ddd0418ce9c841b5b9ca6f5be5603d
José Fernando Chávez Castillo <josefernandochca@gmail.com>
2022-08-18 22:43:10 -0500

    Update

--- cogs/server-info.py
+++ cogs/server-info.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from config import *
 import nextcord
 from nextcord.ext import commands
@@ -16,7 +17,7 @@ class ServerInfo(commands.Cog):
         embed.set_thumbnail(interaction.guild.icon)
         embed.add_field(name='Owner', value=interaction.guild.owner, inline=False)
         embed.add_field(name='Creado', value=interaction.guild.created_at.__format__('%d/%m/%Y, %H:%M:%S PM'), inline=False)
-        #embed.add_field(name='Contador de Miembros', value=f'{interaction.guild.humans}{interaction.guild.bots}', inline=False)
+        embed.add_field(name='Contador de Miembros', text=f'{interaction.guild.humans}{interaction.guild.bots}', inline=False)
         embed.add_field(name='Top rol', value=interaction.guild.roles[-31])
         embed.add_field(name='Bots', value=list_of_bots, inline=False)
         embed.set_footer(text=f'Requested by: {interaction.user.name}#{interaction.user.discriminator}\n{Fecha_actual}', icon_url='https://i.ibb.co/nCCJ2Wb/378-3782140-discord-server-icon-cute-imagenes-para-perfil-de.png')
