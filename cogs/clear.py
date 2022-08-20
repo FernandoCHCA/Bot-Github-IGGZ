0f0da92e95a1e9ff82f74194fa244945af030af7
José Fernando Chávez Castillo <josefernandochca@gmail.com>
2022-08-20 09:45:19 -0500

    Update

--- cogs/clear.py
+++ cogs/clear.py
@@ -10,42 +10,38 @@ class Clear(commands.Cog):
         self.client = client
 
     @bot.slash_command(guild_ids=[ServersID], name='clear', description='Borra la cantidad especifica de mensajes que gustes!')
-    async def Clear(self, interaction: Interaction, amount:str):
+    async def Clear_Slash_Command(self, interaction: Interaction, amount:str):
         if amount == 'all':
             await interaction.channel.purge()
             embed=nextcord.Embed(description="**Se han borrado con exito todos los mensajes**", color=0x1FD3F3)
             await interaction.response.send_message(embed=embed, ephemeral=True)
-            await asyncio.sleep(20.0)
             await interaction.delete_original_message()
         else:
             await interaction.channel.purge(limit=(int(amount)))
             embed=nextcord.Embed(description=f"**Borrado con exito {amount} mensajes**", color=0x1FD3F3)
             await interaction.response.send_message(embed=embed, ephemeral=True)
-            await asyncio.sleep(20.0)
             await interaction.delete_original_message()
 
-# class Clear2(commands.Cog):
-#     def __init__(self, client):
-#         self.client = client
+class Clear2(commands.Cog):
+    def __init__(self, client):
+        self.client = client
 
     @bot.command(guild_ids=[ServersID])
-    async def Clear2(self, ctx: Interaction, amount:str):
+    async def clear(self, interaction: Interaction, amount:str):
         if amount == 'all':
-            await ctx.channel.purge()
+            await interaction.channel.purge()
             embed=nextcord.Embed(description="**Se han borrado con exito todos los mensajes**", color=0x1FD3F3)
-            await ctx.send(embed=embed, ephemeral=True)
-            await asyncio.sleep(20.0)
-            await ctx.delete_original_message()
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+            await interaction.delete_original_message()
         else:
-            await ctx.channel.purge(limit=(int(amount)+1))
+            await interaction.channel.purge(limit=(int(amount)+1))
             embed=nextcord.Embed(description=f"**Borrado con exito {amount} mensajes**", color=0x1FD3F3)
-            await ctx.send(embed=embed, ephemeral=True)
-            await asyncio.sleep(20.0)
-            await ctx.delete_original_message()
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+            await interaction.delete_original_message()
 
 #Setup 
 def setup(client):
-    client.add_cog(Clear(client))
+    client.add_cog(Clear, Clear2(client))
 
 # def setup(client):
 #     client.add_cog(Clear2(client))
