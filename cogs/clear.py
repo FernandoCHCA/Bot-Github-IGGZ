8c7e0db6da1250788b7062e4e0495e27f17e9a97
José Fernando Chávez Castillo <josefernandochca@gmail.com>
2022-08-20 09:31:25 -0500

    Update

--- cogs/clear.py
+++ cogs/clear.py
@@ -24,28 +24,28 @@ class Clear(commands.Cog):
             await asyncio.sleep(20.0)
             await interaction.delete_original_message()
 
-class Clear2(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+# class Clear2(commands.Cog):
+#     def __init__(self, client):
+#         self.client = client
 
-    @bot.command(guild_ids=[ServersID])
-    async def Clear(self, interaction: Interaction, amount:str):
-        if amount == 'all':
-            await interaction.channel.purge()
-            embed=nextcord.Embed(description="**Se han borrado con exito todos los mensajes**", color=0x1FD3F3)
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-            await asyncio.sleep(20.0)
-            await interaction.delete_original_message()
-        else:
-            await interaction.channel.purge(limit=(int(amount)))
-            embed=nextcord.Embed(description=f"**Borrado con exito {amount} mensajes**", color=0x1FD3F3)
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-            await asyncio.sleep(20.0)
-            await interaction.delete_original_message()
+#     @bot.command(guild_ids=[ServersID])
+#     async def Clear(self, interaction: Interaction, amount:str):
+#         if amount == 'all':
+#             await interaction.channel.purge()
+#             embed=nextcord.Embed(description="**Se han borrado con exito todos los mensajes**", color=0x1FD3F3)
+#             await interaction.response.send_message(embed=embed, ephemeral=True)
+#             await asyncio.sleep(20.0)
+#             await interaction.delete_original_message()
+#         else:
+#             await interaction.channel.purge(limit=(int(amount)))
+#             embed=nextcord.Embed(description=f"**Borrado con exito {amount} mensajes**", color=0x1FD3F3)
+#             await interaction.response.send_message(embed=embed, ephemeral=True)
+#             await asyncio.sleep(20.0)
+#             await interaction.delete_original_message()
 
 #Setup 
 def setup(client):
     client.add_cog(Clear(client))
 
-def setup(client):
-    client.add_cog(Clear2(client))
+# def setup(client):
+#     client.add_cog(Clear2(client))
