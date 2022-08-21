bd17690724b4eac109012eb1932d8650eee65612
José Fernando Chávez Castillo <josefernandochca@gmail.com>
2022-08-21 10:01:28 -0500

    Update

--- cogs/moderacion.py
+++ cogs/moderacion.py
@@ -1,3 +1,4 @@
+from logging import exception
 import nextcord
 from config import *
 from nextcord import slash_command, Interaction
@@ -22,39 +23,46 @@ class Moderacion(commands.Cog):
             required=False #This will make this option as a Optional.
         )
     ):
-        if not reason: reason="Ninguna razón"
-        await member.kick(reason=reason)
-        embed=nextcord.Embed(
-            title='Usuario Kickeado!!',
-            color=0xEC2424
-            )
-        embed.add_field(name='Kickeado por', value=ctx.user.mention, inline=False)
-        embed.add_field(name='Miembro', value=member, inline=False)
-        embed.add_field(name='Razón', value=reason, inline=False)
-        embed.set_thumbnail(member.avatar)
-        await ctx.response.send_message(embed=embed)
-        #await ctx.response.send_message(f"{member} ha sido expulsado por {ctx.user.mention} por {reason}")
+        try:
+            if not reason: reason="Ninguna razón"
+            await member.kick(reason=reason)
+            embed=nextcord.Embed(title='Usuario Kickeado!', color=0xEC2424)
+            embed.add_field(name='Kickeado por', value=ctx.user.mention, inline=False)
+            embed.add_field(name='Miembro', value=member, inline=False)
+            embed.add_field(name='Razón', value=reason, inline=False)
+            embed.set_thumbnail(member.avatar)
+            await ctx.response.send_message(embed=embed)
+        except Exception:
+            ErrorEmbed = nextcord.Embed(title="Algo salió mal", description="Hubo un error al intentar realizar este comando.", colour=0xEC2424)
+            ErrorEmbed.timestamp = datetime.datetime.utcnow()
+            await ctx.response.send_message(embed=ErrorEmbed, ephemeral=True)
+            return
 
     @slash_command(name='ban', description='Banea a un miembro', guild_ids=[ServersID])
     async def ban(
         self,
         ctx:Interaction,
         member: nextcord.Member = nextcord.SlashOption(name='member', description='Por favor, seleccione un miembro'),
-        reason: str = nextcord.SlashOption(name='reason', description='Por favor, indique la razón', required=False)
+        reason: str = nextcord.SlashOption(name='reason', description='Por favor, indique la razón', required=False),
+        duration: str = nextcord.SlashOption(name='duration', description='Por favor, indique la duración', required=False)
     ):
-        if not reason: reason="Ninguna razón"
-        if not duration: duration="Tiempo indefinido"
-        await member.kick(reason=reason, duration=duration)
-        embed=nextcord.Embed(
-            title='Nuevo ban!',
-            color=0xEC2424
-            )
-        embed.add_field(name='Kickeado por', value=ctx.user.mention, inline=False)
-        embed.add_field(name='Miembro', value=member, inline=False)
-        embed.add_field(name='Razón', value=reason, inline=False)
-        embed.add_field(name='Duración', value=duration, inline=False)
-        embed.set_thumbnail(member.avatar)
-        await ctx.response.send_message(embed=embed)
+        try:
+            if not reason: reason="Ninguna razón"
+            if not duration: duration="Tiempo indefinido"
+            #if duration: 
+            await member.kick(reason=reason, duration=duration)
+            embed=nextcord.Embed(title='Nuevo ban!', color=0xEC2424)
+            embed.add_field(name='Baneado por', value=ctx.user.mention, inline=False)
+            embed.add_field(name='Miembro', value=member, inline=False)
+            embed.add_field(name='Razón', value=reason, inline=False)
+            embed.add_field(name='Duración', value=duration, inline=False)
+            embed.set_thumbnail(member.avatar)
+            await ctx.response.send_message(embed=embed)
+        except Exception:
+            ErrorEmbed = nextcord.Embed(title="Algo salió mal", description="Hubo un error al intentar realizar este comando.", colour=0xEC2424)
+            ErrorEmbed.timestamp = datetime.datetime.utcnow()
+            await ctx.response.send_message(embed=ErrorEmbed, ephemeral=True)
+            return
 
     @slash_command(name='unban', description='Unbanea a un miembro', guild_ids=[ServersID])
     async def unban(
