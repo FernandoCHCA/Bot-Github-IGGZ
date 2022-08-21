4ca44fd6d47cd76a07911d2a101abf94425136d1
José Fernando Chávez Castillo <josefernandochca@gmail.com>
2022-08-21 16:29:22 -0500

    Update

--- cogs/profile-user.py
+++ cogs/profile-user.py
@@ -10,14 +10,14 @@ class ProfileServer(commands.Cog):
     @bot.slash_command(guild_ids=[ServersID], name='profile-user', description="Mira el perfil de un usuario!")
     async def Profile(self, interaction: Interaction, user:nextcord.Member):
         embed=nextcord.Embed(title="Informacion del usuario", color=0x1FD3F3)
-        embed.add_field(name='Usuario', value=user.mention, inline=True)
-        embed.add_field(name='ID', value=user.id, inline=True)
+        embed.add_field(name='Usuario', value=user.mention, inline=False)
+        embed.add_field(name='ID', value=user.id, inline=False)
         embed.add_field(name='Registrado', value=user.created_at.__format__('%d/%m/%Y, %H:%M:%S'), inline=False)
-        embed.add_field(name='Se unio', value=user.joined_at.__format__('%d/%m/%Y, %H:%M:%S'), inline=True)
-        embed.add_field(name='Rol mas alto', value=user.roles[-2], inline=True)
+        embed.add_field(name='Se unio', value=user.joined_at.__format__('%d/%m/%Y, %H:%M:%S'), inline=False)
+        embed.add_field(name='Rol mas alto', value=user.roles[-2], inline=False)
         embed.set_thumbnail(user.avatar)
         embed.set_footer(text=f'Requested by: {interaction.user.name}#{interaction.user.discriminator}\n{Fecha_actual}', icon_url='https://i.ibb.co/nCCJ2Wb/378-3782140-discord-server-icon-cute-imagenes-para-perfil-de.png')
-        embed.set_image(url='https://i.ibb.co/XzNGGC4/Cartel-servidor.png')
+        embed.set_image(url='https://i.image.png.co/XzNGGC4/Cartel-servidor.png')
         #Profile_embed.set_image(url='https://i.ibb.co/DzXLq1K/kgCYlv0.gif') # GIF DE LINEA DE COLORES
         await interaction.response.send_message(embed=embed)
 
