0e2e26c59131b752239edf4cf1a9b67caf98d909
José Fernando Chávez Castillo <josefernandochca@gmail.com>
2022-08-27 17:07:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -36,13 +36,13 @@ class EmbedModal(nextcord.ui.Modal):
         self.emDesc = nextcord.ui.TextInput(label = "Embed Description", min_length = 5, max_length = 4000, required = True, placeholder = "Introduzca la descripcion del embed aquí!", style = nextcord.TextInputStyle.paragraph)
         self.add_item(self.emDesc)
 
-        self.emColor = nextcord.ui.TextInput(label = "Embed Color", min_length = 6, max_length = 6, required = False, placeholder = "Introduzca el color en hexadecimal del embed aquí!")
+        self.emColor : int = nextcord.ui.TextInput(label = "Embed Color", min_length = 6, max_length = 6, required = False, placeholder = "Introduzca el color en hexadecimal del embed aquí!")
         self.add_item(self.emColor)
 
     async def callback(self, interaction: Interaction) -> None:
         title = self.emTitle.value
         description = self.emDesc.value
-        colour = '0x' + self.emColor.value
+        colour = int('0x') + self.emColor.value
         # if colour:
         #     embed.colour=int("0x" + colour, 16)
         #     colour = self.emColor.value
