4bcf550ffa90416356043ec2e075be1facef0285
José Fernando Chávez Castillo <josefernandochca@gmail.com>
2022-08-27 17:26:30 -0500

    Update

---
+++ embed-modals.py
@@ -0,0 +1,39 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord import Embed, Interaction, slash_command, Member, SlashOption, ChannelType
+from config import *
+
+class EmbedModal(nextcord.ui.Modal):
+    def __init__(self):
+        super().__init__(
+            "Embed Maker",
+        )
+
+        self.emTitle = nextcord.ui.TextInput(label = "Embed Title", min_length = 2, max_length = 124, required = True, placeholder = "Introduzca el título del embed aquí!")
+        self.add_item(self.emTitle)
+
+        self.emDesc = nextcord.ui.TextInput(label = "Embed Description", min_length = 5, max_length = 4000, required = True, placeholder = "Introduzca la descripcion del embed aquí!", style = nextcord.TextInputStyle.paragraph)
+        self.add_item(self.emDesc)
+
+        self.emColor : int = nextcord.ui.TextInput(label = "Embed Color", min_length = 6, max_length = 6, required = False, placeholder = "Introduzca el color en hexadecimal del embed aquí!")
+        self.add_item(self.emColor)
+
+    async def callback(self, interaction: Interaction) -> None:
+        title = self.emTitle.value
+        description = self.emDesc.value
+        colour = int('0x' + self.emColor.value, 16)
+        embed = nextcord.Embed(title=title, description=description, colour=colour)
+        # embed = nextcord.Embed(title=title, description=description, colour=nextcord.Color.random())
+        return await interaction.response.send_message(embed=embed)
+
+class Embed_Modals_Cog(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @bot.slash_command(name="embed", description="Crea un Embed personalizado!", guild_ids=[ServersID])
+    async def embed(interaction: Interaction):
+        await interaction.response.send_modal(EmbedModal())
+
+#Setup 
+def setup(client):
+    client.add_cog(Embed_Modals_Cog(client))
--- main.py
+++ main.py
@@ -24,35 +24,32 @@ async def on_ready():
     print(f'          El bot {bot.user} esta en linea...')
     print('------------------------------------------------------------\n')
 
-class EmbedModal(nextcord.ui.Modal):
-    def __init__(self):
-        super().__init__(
-            "Embed Maker",
-        )
+# class EmbedModal(nextcord.ui.Modal):
+#     def __init__(self):
+#         super().__init__(
+#             "Embed Maker",
+#         )
 
-        self.emTitle = nextcord.ui.TextInput(label = "Embed Title", min_length = 2, max_length = 124, required = True, placeholder = "Introduzca el título del embed aquí!")
-        self.add_item(self.emTitle)
+#         self.emTitle = nextcord.ui.TextInput(label = "Embed Title", min_length = 2, max_length = 124, required = True, placeholder = "Introduzca el título del embed aquí!")
+#         self.add_item(self.emTitle)
 
-        self.emDesc = nextcord.ui.TextInput(label = "Embed Description", min_length = 5, max_length = 4000, required = True, placeholder = "Introduzca la descripcion del embed aquí!", style = nextcord.TextInputStyle.paragraph)
-        self.add_item(self.emDesc)
+#         self.emDesc = nextcord.ui.TextInput(label = "Embed Description", min_length = 5, max_length = 4000, required = True, placeholder = "Introduzca la descripcion del embed aquí!", style = nextcord.TextInputStyle.paragraph)
+#         self.add_item(self.emDesc)
 
-        self.emColor : int = nextcord.ui.TextInput(label = "Embed Color", min_length = 6, max_length = 6, required = False, placeholder = "Introduzca el color en hexadecimal del embed aquí!")
-        self.add_item(self.emColor)
+#         self.emColor : int = nextcord.ui.TextInput(label = "Embed Color", min_length = 6, max_length = 6, required = False, placeholder = "Introduzca el color en hexadecimal del embed aquí!")
+#         self.add_item(self.emColor)
 
-    async def callback(self, interaction: Interaction) -> None:
-        title = self.emTitle.value
-        description = self.emDesc.value
-        colour = int('0x' + self.emColor.value, 16)
-        # if colour:
-        #     embed.colour=int("0x" + colour, 16)
-        #     colour = self.emColor.value
-        embed = nextcord.Embed(title=title, description=description, colour=colour)
-        # embed = nextcord.Embed(title=title, description=description, colour=nextcord.Color.random())
-        return await interaction.response.send_message(embed=embed)
+#     async def callback(self, interaction: Interaction) -> None:
+#         title = self.emTitle.value
+#         description = self.emDesc.value
+#         colour = int('0x' + self.emColor.value, 16)
+#         embed = nextcord.Embed(title=title, description=description, colour=colour)
+#         # embed = nextcord.Embed(title=title, description=description, colour=nextcord.Color.random())
+#         return await interaction.response.send_message(embed=embed)
 
-@bot.slash_command(name="embed", description="Crea un Embed personalizado!", guild_ids=[ServersID])
-async def embed(interaction: Interaction):
-    await interaction.response.send_modal(EmbedModal())
+# @bot.slash_command(name="embed", description="Crea un Embed personalizado!", guild_ids=[ServersID])
+# async def embed(interaction: Interaction):
+#     await interaction.response.send_modal(EmbedModal())
 
 #Cogs
 for fn in os.listdir('./cogs'):
