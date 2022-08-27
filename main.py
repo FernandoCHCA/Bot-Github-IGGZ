19e513e4d86ed397ee5bf9fba7de1d972d883d18
José Fernando Chávez Castillo <josefernandochca@gmail.com>
2022-08-27 11:14:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ class EmbedModal(nextcord.ui.Modal):
     async def callback(self, interaction: Interaction) -> None:
         title = self.emTitle.value
         description = self.emDesc.value
-        colour = self.emColor.value
         if colour:
             embed.colour=int("0x" + colour, 16)
-            embed = nextcord.Embed(title=title, description=description, colour=colour)
+            colour = self.emColor.value
+        embed = nextcord.Embed(title=title, description=description, colour=colour)
         return await interaction.response.send_message(embed=embed)
 
 @bot.slash_command(name="embed", description="Crea un Embed personalizado!", guild_ids=[ServersID])
